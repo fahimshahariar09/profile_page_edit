33cc3798d606482c38a063172880ade04443b39d
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-27 23:36:03 -0800

    first commit

---
+++ lib/view/common_widget/common_text.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class CommonText extends StatelessWidget {
+  const CommonText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("data");
+  }
+}
