51ef4aecf6ac7515b92e8cc41792ff4d5883f176
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-27 23:23:12 -0800

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:profile/view/screen/profile_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,13 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+      home: ProfilePage(),
     );
   }
 }
-
