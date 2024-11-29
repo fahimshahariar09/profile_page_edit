1ab59614d2df4e7fef5e35d7785db831eb1fdea6
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-28 23:33:57 -0800

    first commit

--- lib/controller/local_storage/local_storage.dart
+++ lib/controller/local_storage/local_storage.dart
@@ -1,19 +1,44 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-// Create storage
-final storage = new FlutterSecureStorage();
+class LocalData{
 
-// Read value
-String value = await storage.read(key: key);
+  final storage = FlutterSecureStorage();
 
-// Read all values
-Map<String, String> allValues = await storage.readAll();
+  // Write Data in Local Storage
+  writeData({required String key, required String value}) async {
+    await storage.write(
+      key: key,
+      value: value,
+    );
+    debugPrint("Data Stored Successfully.");
+  }
 
-// Delete value
-await storage.delete(key: key);
+  // Read Data in Local Storage
+  Future<String> readData({required String key}) async {
+    var a = await storage.read(key: key);
+    debugPrint("Local Data :  $a");
+    return a ?? "";
+  }
 
-// Delete all
-await storage.deleteAll();
+  // Read All Data in Local Storage
+  Future<Map<String, String>> readAllData() async {
+    var a = await storage.readAll();
+    debugPrint("All Local Data :  $a");
+    return a;
+  }
 
-// Write value
-await storage.write(key: key, value: value);
+  // Delete Specific Data from Local Storage
+  deleteData({required String key}) async {
+    await storage.delete(key: key);
+    debugPrint("Local Data Deleted.!");
+  }
+
+  // Delete All Data from Local Storage
+  deleteAllData() async {
+    await storage.deleteAll();
+    debugPrint("All Local Data Deleted..!!");
+  }
+
+}
+
