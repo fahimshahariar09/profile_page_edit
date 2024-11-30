c2dee61dcffebc85713c8d74c3c5cb480ef10ea9
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-29 23:31:55 -0800

    first commit

--- lib/view/common_widget/custom_loading_button.dart
+++ lib/view/common_widget/custom_loading_button.dart
@@ -7,8 +7,21 @@ class CustomLoadingButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 15),
       color: AppColors.bg1LightColor,
-      shape: OutlineInputBorder(borderRadius: BorderRadius.circular(5),borderSide: BorderSide.none),
+      shape: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(5),
+        borderSide: BorderSide.none,
+      ),
+      child: Padding(padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 15),
+      child: SizedBox(
+        height: 35,
+        width: MediaQuery.sizeOf(context).width,
+        child: const CircularProgressIndicator(
+          backgroundColor: Colors.white,
+          color: AppColors.bg1LightColor,
+        ),
+      ),),
     );
   }
 }
