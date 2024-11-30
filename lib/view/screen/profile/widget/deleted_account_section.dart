b638e66bb5b10ed32ee2e0ee53b6661db794eaed
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-29 23:53:56 -0800

    first commit

--- lib/view/screen/profile/widget/deleted_account_section.dart
+++ lib/view/screen/profile/widget/deleted_account_section.dart
@@ -1,24 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:profile/controller/ui_controller/profile.dart';
+import 'package:profile/utlis/common_funcation/common_snackbar_message.dart';
+import 'package:profile/utlis/common_funcation/internet_connection_checkout.dart';
 import 'package:profile/utlis/theme/app_colors.dart';
+import 'package:profile/view/common_widget/custom_button.dart';
+import 'package:profile/view/common_widget/custom_loading_button.dart';
+import 'package:profile/view/screen/profile/widget/custom_text_field.dart';
 
 class DeletedAccountSection extends StatelessWidget {
   const DeletedAccountSection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.account_circle,
-              color: AppColors.bg1LightColor,
-            ),
-            SizedBox(height: 10),
-          ],
-        )
-      ],
+    ProfileController profileController = Get.put(ProfileController());
+    return Padding(
+      padding: const EdgeInsets.only(top: 20, right: 10, left: 10, bottom: 10),
+      child: SingleChildScrollView(
+        child: Form(
+          key: profileController.formKey,
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.account_circle,
+                    color: AppColors.bg1LightColor,
+                  ),
+                  const SizedBox(height: 10),
+                  CustomTextField(
+                    controller: profileController.oldPasswordController,
+                    labelText: "current_password",
+                  ),
+                  const SizedBox(height: 10),
+                  Obx(() => profileController.isLoading.value
+                      ? const CustomLoadingButton()
+                      : CustomButton(
+                          text: "deleted",
+                          buttonWidth: 150,
+                          onTap: () async {
+                            FocusScope.of(context).unfocus();
+                            if (!profileController.formKey.currentState!
+                                .validate()) {
+                              return;
+                            }
+                            if (!await ConnectionChecker.checkConnection()) {
+                              CommonSnackBarMessage.noInternetConnection();
+                              return;
+                            }
+                            bool status =
+                                await profileController.accountDeleteService();
+                            if (status) {
+                              profileController.oldPasswordController.clear();
+                              Get.back();
+                            }
+                          }))
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
