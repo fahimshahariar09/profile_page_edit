04b9e893244e068d58119f048d03a82cca9def5d
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-30 23:25:30 -0800

    first commit

---
+++ lib/view/screen/profile/widget/profile_edit_section.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class ProfileEditSection extends StatelessWidget {
+  const ProfileEditSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+      ],
+    );
+  }
+}
