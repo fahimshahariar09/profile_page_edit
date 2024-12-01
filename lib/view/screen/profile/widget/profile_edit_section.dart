38c372e638f86c3e70b59e9d7d25da67f3ef424d
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-30 23:32:18 -0800

    first commit

--- lib/view/screen/profile/widget/profile_edit_section.dart
+++ lib/view/screen/profile/widget/profile_edit_section.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:profile/controller/ui_controller/profile.dart';
+import 'package:profile/view/common_widget/custom_text.dart';
 
 class ProfileEditSection extends StatelessWidget {
   const ProfileEditSection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-
-      ],
+    ProfileController profileController = Get.put(ProfileController());
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10, right: 10, left: 10, top: 10),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CustomTextWidget(text: "profile edit",fontSize: 20,fontWeight: FontWeight.w500,)
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
