c2505ebf0289b28e1e43801d303387523633de24
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-02 23:36:24 -0800

    first commit

--- lib/controller/api_controller/auth/log_out.dart
+++ lib/controller/api_controller/auth/log_out.dart
@@ -1,1 +1,9 @@
-class LogOutService{}
+import 'dart:nativewrappers/_internal/vm/lib/developer.dart';
+
+class LogOutService{
+
+  static Future<bool> logoutService()async{
+    try{}catch(e){log("error $e");} return false;
+  }
+
+}
