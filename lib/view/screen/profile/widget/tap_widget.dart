2cf3d36d2073833e0c6117255297341ae134103b
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-03 00:10:04 -0800

    first commit

--- lib/view/screen/profile/widget/tap_widget.dart
+++ lib/view/screen/profile/widget/tap_widget.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:profile/utlis/theme/app_colors.dart';
+import 'package:profile/view/common_widget/custom_text.dart';
 
 class TapWidget extends StatelessWidget {
   const TapWidget(
       {super.key,
       required this.onTap,
       this.value,
-      required this.selectedIndex});
+      required this.selectedIndex,
+      required this.titel});
 
   final int selectedIndex;
+  final String titel;
   final VoidCallback onTap;
   final dynamic value;
 
@@ -21,6 +24,9 @@ class TapWidget extends StatelessWidget {
             ? AppColors.bgLightColor
             : AppColors.bg1LightColor,
         margin: EdgeInsets.zero,
+        shape: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(5),
+            borderSide: BorderSide(color: Colors.transparent)),
         child: SizedBox(
           height: 400,
           width: 300,
@@ -32,6 +38,8 @@ class TapWidget extends StatelessWidget {
                     ? const Icon(Icons.check_box_rounded,
                         size: 20, color: AppColors.bg1LightColor)
                     : const SizedBox(),
+                SizedBox(width: 5),
+                Flexible(child: CustomTextWidget(text: titel))
               ],
             ),
           ),
