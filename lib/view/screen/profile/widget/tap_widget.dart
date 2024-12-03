2df40fc27244ca1c635f46efc1113869197edd5c
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-02 23:59:03 -0800

    first commit

--- lib/view/screen/profile/widget/tap_widget.dart
+++ lib/view/screen/profile/widget/tap_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:profile/utlis/theme/app_colors.dart';
 
 class TapWidget extends StatelessWidget {
   const TapWidget({super.key, required this.onTap});
@@ -10,6 +12,9 @@ class TapWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
+      child: Card(
+        color: AppColors.bg1LightColor,
+      ),
     );
   }
 }
