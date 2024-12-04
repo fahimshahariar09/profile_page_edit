1ae5092dd631e5e0843bfad2622c9556b7373c8e
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-03 22:47:17 -0800

    first commit

--- lib/view/common_widget/custom_rating.dart
+++ lib/view/common_widget/custom_rating.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/cupertino.dart';
 
 class CustomRating extends StatelessWidget {
-  const CustomRating({super.key});
+  const CustomRating({super.key, required this.ratingStar});
+
+  final int ratingStar;
 
   @override
   Widget build(BuildContext context) {
-    return Row();
+
+    int ratingCount =0;
+    if()
+
+
+    return SingleChildScrollView(
+      child: Row(
+        children: [
+          SizedBox(
+
+          )
+        ],
+      ),
+    );
   }
 }
